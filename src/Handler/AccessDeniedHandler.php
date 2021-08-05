6bff345ae1ba45b3db93ad82a356c317c2d500e8
Aldwinx <quentinpinchart@gmail.com>
2021-08-05 15:51:42 +0200

    AccessDeniedHandler -> Fait un message flash puis redirige si l'utilisateur n'a pas les droits pour accèder à une page (n'a pas le rôle nécessaire). Lien absolu à modifier si nécessaire

---
+++ src/Handler/AccessDeniedHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Handler;
+
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $request->getSession()->getFlashBag()->add('access_denied', 'Vous n\'avez pas les permissions pour accèder à cette page');
+        return new RedirectResponse('http://localhost/Symfony_Projet_Sorties/public/');
+    }
+}
